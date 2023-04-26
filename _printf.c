cf91323f4afb26b996cbfc08bccf641dbe658408
asma boutagayout <asmaboutag@gmail.com>
2023-04-26 11:46:27 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -9,8 +9,9 @@ int _printf(const char *format, ...)
 va_list para_list;
 int i = 0, length = 0, j;
 group argums[] = {
-	{"%c", print_char}, {"%i", print_int},
-	{"%d", print_dec}, {"%%", print_percent}
+	{"%c", print_char}, {"%s", print_string},
+	{"%i", print_int}, {"%d", print_dec},
+	{"%%", print_percent}
 	};
 if (format == NULL)
 return (-1);
@@ -20,7 +21,7 @@ return (-1);
 A:
 while (format[i] != '\0')
 {
-j = 3;
+j = 4;
 while (j >= 0)
 {
 if (argums[j].spe[0] == format[i] && argums[j].spe[1] == format[i + 1])
