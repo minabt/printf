e5c2807b1ebb12984e1898c671ddd40181b2ef09
asma boutagayout <asmaboutag@gmail.com>
2023-04-25 18:46:52 -0700

    _printf()

--- _printf.c
+++ _printf.c
@@ -1,59 +1,41 @@
 #include "main.h"
-void print_buffer(char buffer[], int *buff_ind);
 /**
-* _printf - Printf function
-* @format: format.
-* Return: Printed chars.
+ * _printf - print function that produces output according to a format.
+ * @format: character string
+ * Return:  the number of characters printed
 */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-	va_start(list, format);
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
-printed_chars++;
-				}
-				else
-				{
-					print_buffer(buffer, &buff_ind);
-					flags = get_flags(format, &i);
-					width = get_width(format, &i, list);
-					precision = get_precision(format, &i, list);
-					size = get_size(format, &i);
-					++i;
-					printed = handle_print(format, &i, list, buffer,
-							flags, width, precision, size);
-					if (printed == -1)
-						return (-1);
-					printed_chars += printed;
-				}
-			}
-	print_buffer(buffer, &buff_ind);
-	va_end(list);
-	return (printed_chars);
-}
-
-/**
-* print_buffer - Prints the contents of the buffer if it exist
-* @buffer: Array of chars
-* @buff_ind: Index at which to add next char, represents the length.
-*/
-void print_buffer(char buffer[], int *buff_ind)
+va_list para_list;
+int i = 0, length = 0, j;
+igroup argums[] = {
+	{"%c", print_char}, {"%s", print_string},
+	{"%i", print_int}, {"%d", print_dec},
+	{"%%", print_percent}
+	};
+if (format == NULL)
+return (-1);
+va_start(para_list, format);
+if (format[0] == '%' && format[1] == '\0')
+return (-1);
+A:
+while (format[i] != '\0')
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-	*buff_ind = 0;
+j = 4;
+while (j >= 4)
+{
+if (argums[j].spe[0] == format[i] && argums[j].spe[1] == format[i + 1])
+{
+length += argums[j].func(para_list);
+i = i + 2;
+goto A;
 }
-
+j--;
+}
+_putchar(format[i]);
+length++;
+i++;
+}
+va_end(para_list);
+return (length);
+}
